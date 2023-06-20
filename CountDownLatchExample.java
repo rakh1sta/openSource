b713dcb40afa1f7973b4e0fc58f29447475f188f
Rakhimova Shokhista <85543690+binti-jamshidbek@users.noreply.github.com>
2023-06-20 14:04:59 +0500

    Create CountDownLatchExample.java

---
+++ CountDownLatchExample.java
@@ -0,0 +1,40 @@
+public class CountDownLatchExample  { 
+    public static void main(String args[]) throws InterruptedException {  
+        CountDownLatch latch = new CountDownLatch(4);  
+  
+        Worker thread1 = new Worker(1000, latch, "Worker Thread-1");  
+        Worker thread2 = new Worker(2000, latch, "Worker Thread-2");  
+        Worker thread3 = new Worker(3000, latch, "Worker Thread-3");  
+        Worker thread4 = new Worker(4000, latch, "Worker Thread-4");  
+       
+        thread1.start();  
+        thread2.start();  
+        thread3.start();  
+      
+        thread4.start();  
+        System.out.println("Main thread waiting......");
+        latch.await();  
+        System.out.println("Thread "+Thread.currentThread().getName()+" finished");  
+    }  
+}  
+ 
+class Worker extends Thread  { 
+    private int delay;  
+    private CountDownLatch latch; 
+     
+    public Worker(int delay, CountDownLatch latch, String name)  {  
+        super(name);  
+        this.delay = delay;  
+        this.latch = latch;  
+    }  
+    @Override  
+    public void run()  {   
+        try {  
+            Thread.sleep(delay);  
+            latch.countDown();
+            System.out.println(Thread.currentThread().getName() + " has finished");  
+        }catch (InterruptedException e){  
+            e.printStackTrace();  
+        }  
+    }  
+}  
